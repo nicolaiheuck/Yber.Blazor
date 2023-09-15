72d32b1fd3cc29d153ca0cd711c52562182916dc
Googlelai <nicolaiheuck@gmail.com>
2023-09-15 08:47:17 +0200

    Registered services in Program.cs

--- Yber.Blazor/Program.cs
+++ Yber.Blazor/Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
 using Yber.Blazor.Data;
+using Yber.Blazor.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,6 +9,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 builder.Services.AddSingleton<WeatherForecastService>();
+builder.RegisterDependencies("Yber.Blazor.Services");
 
 var app = builder.Build();
 
