be285e5857f0c4422b515742cdf95761dc2136dd
Jan Andreasen <jan@tved.it>
2023-09-20 11:04:00 +0200

    Moving to Azure keyvault

--- Yber.API/Program.cs
+++ Yber.API/Program.cs
@@ -1,31 +1,54 @@
-using System.Reflection;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Yber.API.Extensions;
 using Yber.Repositories.DBContext;
 using Yber.Services.Interfaces;
 using Yber.Services.Services;
+using Azure.Identity;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Microsoft.Identity.Web;
+using Microsoft.Identity.Web.UI;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// Add authentication and authorization for API endpoints
+builder.Services.AddAuthentication(OpenIdConnectDefaults.AuthenticationScheme)
+    .AddMicrosoftIdentityWebApi(builder.Configuration.GetSection("AzureAd"));
+
+// Add authentication and authorization for Blazor Server app
+builder.Services.AddAuthentication(OpenIdConnectDefaults.AuthenticationScheme)
+    .AddMicrosoftIdentityWebApp(builder.Configuration.GetSection("AzureAd"));
+
+builder.Services.AddControllersWithViews()
+    .AddMicrosoftIdentityUI();
+
+builder.Services.AddAuthorization(options =>
+{
+    // By default, all incoming requests will be authorized according to the default policy
+    options.FallbackPolicy = options.DefaultPolicy;
+});
+
+var keyVaultEndpoint = new Uri(Environment.GetEnvironmentVariable("VaultUri"));
+builder.Configuration.AddAzureKeyVault(keyVaultEndpoint, new DefaultAzureCredential());
+// BUG AZURE KEYVAULT
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerWithOAuth();
+// builder.Services.AddSwaggerWithOAuth(); // TODO REMOVE OAuth SWAGGER
 builder.RegisterDependencies();
 builder.Services.AddDbContext<YberContext>();
 // builder.Services.AddScoped<IYberService, YberService>();
 
-// Authentication pipeline config - START
-builder.Services.AddAuthentication(options =>
-{
-    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-}).AddJwtBearer(options =>
-{
-    options.Authority = "https://tved-it.eu.auth0.com/";
-    options.Audience = "https://uber.travel";
-});
-// Authentication pipeline config - END
+// // Authentication pipeline config - START
+// builder.Services.AddAuthentication(options =>
+// {
+//     options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+//     options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+// }).AddJwtBearer(options =>
+// {
+//     options.Authority = "https://tved-it.eu.auth0.com/";
+//     options.Audience = "https://uber.travel";
+// });
+// // Authentication pipeline config - END
 
 var app = builder.Build();
 
