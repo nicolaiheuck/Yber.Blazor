7df1963cfc7e8961e9c04b1118da9a08e6302a96
nicolai heuck <nicolaiheuck@gmail.com>
2023-09-24 19:04:22 +0200

    Added logging

--- Yber.API/Controllers/CarpoolController.cs
+++ Yber.API/Controllers/CarpoolController.cs
@@ -10,24 +10,55 @@ namespace Yber.API.Controllers;
 [Route("[controller]/[action]")]
 public class CarpoolController : ControllerBase
 {
+    private readonly ILogger<CarpoolController> _logger;
+
+    public CarpoolController(ILogger<CarpoolController> logger)
+    {
+        _logger = logger;
+    }
+
     [HttpGet("/GetStudentLift")]
     public async Task<List<StudentDTO>> GetStudentLiftLocationsAsync([FromServices] IYberService service)
     {
-        var studentLifts = await service.GetStudentRequesterLocationInfoAsync();
-        return studentLifts;
+        try
+        {
+            var studentLifts = await service.GetStudentRequesterLocationInfoAsync();
+            return studentLifts;
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, "GetStudentLiftLocationsAsync failed");
+            throw;
+        }
     }
 
     [HttpGet("/GetStudentDriver")]
     public async Task<List<StudentDTO>> GetStudentDriverLocationsAsync([FromServices] IYberService service)
     {
-        var studentDriver = await service.GetStudentDriverLocationInfoAsync();
-        return studentDriver;
+        try
+        {
+            var studentDriver = await service.GetStudentDriverLocationInfoAsync();
+            return studentDriver;
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, "GetStudentDriverLocationsAsync failed");
+            throw;
+        }
     }
 
     [HttpPost("/GetStudentRoute")]
     public async Task<CalculatedRouteDTO> GetCurrentStudentRouteInfoAsync([FromServices] IYberService service, string studentName)
     {
-        var studentInfo = await service.GetEncodedRouteLineAsync(studentName);
-        return studentInfo;
+        try
+        {
+            var studentInfo = await service.GetEncodedRouteLineAsync(studentName);
+            return studentInfo;
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, "GetCurrentStudentRouteInfoAsync failed");
+            throw;
+        }
     }
 }
